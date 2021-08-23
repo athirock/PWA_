54481be58345f4d35c471aad09134d81768a2633
athi <dilorensgon@gmail.com>
2021-08-23 11:58:27 +0200

    btn 2

--- install.js
+++ install.js
@@ -1,6 +1,14 @@
 const installBtn = document.querySelector('.install')
+let dererredPrompt 
 
-window.addEventListener('beforeinstallprompt', (e)=>{
+window.addEventListener('beforeinstallprompt', (e) => {
     e.preventDefault()
+    dererredPrompt = e
     installBtn.classList.remove('hidden')
+})
+
+installBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    installBtn.classList.add('hidden')
+    dererredPrompt.prompt()
 })
